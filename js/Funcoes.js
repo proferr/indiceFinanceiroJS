401b485d5eb26eafa7815fc7ed112d48ba5ed863
Fernando Pereira <fernando.pereira@01tec.com.br>
2022-05-24 19:15:40 -0300

    remove files

--- js/Funcoes.js
+++
@@ -1,7 +0,0 @@
-//VALIDAÇÕES
-$('.validaNumero').live('keypress', function(event) {
-    return (soNums(event,''));
-});
-$('.validaFormato').live('keydown', function(event) {
-    Formata(this,20,event,2);
-});
