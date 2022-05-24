7232774ef9aedc5be198c55ee8ee7b4e0b5e2d77
Fernando Pereira <fernando.pereira@01tec.com.br>
2022-05-24 19:14:55 -0300

    simplify code

--- js/ValidarIndiceFinanceiro.js
+++ js/ValidarIndiceFinanceiro.js
@@ -53,15 +53,6 @@ function ConverterMoedaDecimais(num)
 	    j = (j = i.length) > 3 ? j % 3 : 0;
 	   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 	}
-function DesFormataMoeda(valor)
-	{
-		if(valor == undefined){ valor = "0,00"; }
-		valor = valor.replace(".","").replace(".","");
-		valor = valor.replace(",",".");
-		return valor;
-	}
-
-	
 function Limpar(valor, validos)
 	{ // retira caracteres invalidos da string 
 		var result = ""; 
